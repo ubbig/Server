a5aacaa3093b732a95cb2c707f8f83adabf94eb3
young jun kim <yjkim@selab.co.kr>
2020-01-10 09:55:15 +0900

    centos8 docker설치 스크립트 추가

---
+++ Centos/docker_install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf list docker-ce
+sudo dnf install docker-ce --nobest -y
+sudo systemctl start docker
+sudo systemctl enable docker
