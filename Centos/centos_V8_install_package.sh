e66c8e00f934b231ba6fd839a515e2f249af1a57
yjkim <yjkim@selab.co.kr>
2020-01-15 17:04:05 +0900

    centos설치 스크립트 개발툴, 모니터링 툴 추가

--- Centos/centos_V8_install_package.sh
+++ Centos/centos_V8_install_package.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+# x window
 sudo yum groupinstall -y "Server with GUI"
-
 sudo systemctl set-default graphical
+sudo dnf install -y telnet
 
-sudo dnf isntall -y telnet
+# monitoring
+sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo dnf update -y
+sudo dnf install -y htop sysstat
+
+# development
+sudo dnf install -y git svn
