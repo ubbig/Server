994535d8c4f570fe20b48e5b5b3909219ffa129e
young jun kim <yjkim@selab.co.kr>
2020-01-17 10:40:31 +0900

    .net core 설치스크립트 추

---
+++ Centos/install_dotnetcore.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo rpm -Uvh https://packages.microsoft.com/config/centos/7/packages-microsoft-prod.rpm
+sudo yum install -y dotnet-sdk-3.1
+sudo yum install -y dotnet-sdk-2.1
--- Docker/install_mysql.sh
+++ Docker/install_mysql.sh
@@ -4,5 +4,7 @@ docker pull mysql:8.0.18
 
 docker run -d --restart unless-stopped --name docker_mysql -p 3306:3306 -v /docker_data/docker_mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=selab mysql:8.0.18
 
+# TODO : 외부 asscess 설정 추가
+
 sudo firewall-cmd --permanent --zone=public --add-port=3306/tcp
 sudo firewall-cmd --reload
