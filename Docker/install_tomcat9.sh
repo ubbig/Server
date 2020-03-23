1eba876103d79c5c91ba0f8f9ae5635a0f05c026
김영준 <yjkim@selab.co.kr>
2020-03-23 13:17:48 +0900

    Update install_tomcat9.sh

--- Docker/install_tomcat9.sh
+++ Docker/install_tomcat9.sh
@@ -2,7 +2,7 @@
 
 docker pull tomcat:9.0-jdk11-adoptopenjdk-hotspot
 
-docker run -dit --name tomcat9 -p 8080:8080 tomcat:9.0-jdk11-adoptopenjdk-hotspot
+docker run -dit --restart unless-stopped --name tomcat9 -p 8080:8080 tomcat:9.0-jdk11-adoptopenjdk-hotspot
 
 sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
 sudo firewall-cmd --reload
