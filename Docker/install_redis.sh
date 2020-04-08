5aeab6ce8d1dfac8534f1a4fc8889422f9fc8d49
김영준 <ygkim@selab.co.kr>
2020-04-08 11:13:05 +0900

    redis add

---
+++ Docker/install_redis.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker run -dit --restart unless-stopped --name redis --cpus=1 -p 6379:6379 redis:5.0.8 redis-server --appendonly yes
+
+firewall-cmd --permanent --zone=public --add-port=6379/tcp
+firewall-cmd --reload
