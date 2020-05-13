ae9aa6d05f8833daa2049602bfc47b35ac6a5a98
김영준 <ygkim@selab.co.kr>
2020-05-13 11:47:05 +0900

    openjdk다운로드 경로 목록 정리

---
+++ Etc/openjdk_download.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# openjdk
+wget https://download.java.net/openjdk/jdk7u75/ri/jdk_ri-7u75-b13-linux-x64-18_dec_2014.tar.gz
+wget https://download.java.net/openjdk/jdk8u41/ri/openjdk-8u41-b04-linux-x64-14_jan_2020.tar.gz
+wget https://download.java.net/openjdk/jdk11/ri/openjdk-11+28_linux-x64_bin.tar.gz
+
+# tomcat
+http://mirror.apache-kr.org/tomcat/tomcat-9/v9.0.34/bin/apache-tomcat-9.0.34.tar.gz
+http://mirror.navercorp.com/apache/tomcat/tomcat-8/v8.5.54/bin/apache-tomcat-8.5.54.tar.gz
+http://mirror.navercorp.com/apache/tomcat/tomcat-7/v7.0.103/bin/apache-tomcat-7.0.103.tar.gz
