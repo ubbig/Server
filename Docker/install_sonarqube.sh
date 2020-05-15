587ceec4535169a4e81523d521405ebc385a31c4
김영준 <yjkim@selab.co.kr>
2020-05-15 13:23:38 +0900

    소나큐브 설치 스크립트 추가

--- Docker/install_sonarqube.sh
+++ Docker/install_sonarqube.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo sysctl -w vm.max_map_count=262144
-sudo sysctl -w fs.file-max=65536
-sudo ulimit -n 65536
-sudo ulimit -u 4096
+sudo sysctl -w vm.max_map_count=262144  # linux vm memory limit
+sudo sysctl -w fs.file-max=65536  # max file descriptor
+sudo ulimit -n 65536  # open file descriptor
+sudo ulimit -u 4096   # user process count limit
 
 docker pull sonarqube:7.9.3-community
 
