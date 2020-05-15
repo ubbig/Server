306fc4c137d54c1c95d03db9755362f67e4abdf9
김영준 <yjkim@selab.co.kr>
2020-05-15 13:19:40 +0900

    소나큐브 설치 스크립트 추가

--- Docker/install_sonarqube.sh
+++ Docker/install_sonarqube.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+sudo sysctl -w vm.max_map_count=262144
+sudo sysctl -w fs.file-max=65536
+sudo ulimit -n 65536
+sudo ulimit -u 4096
+
 docker pull sonarqube:7.9.3-community
 
 docker run -d --restart unless-stopped --name sonarqube -p 9000:9000 sonarqube:7.9.3-community
+
