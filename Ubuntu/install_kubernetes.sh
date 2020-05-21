13c82bf2aad37c8c47448a1a6ca304e9dad9b5ac
김영준 <yjkim@selab.co.kr>
2020-05-21 10:35:11 +0900

    쿠버네티스 대시보드 설치 추가

--- Ubuntu/install_kubernetes.sh
+++ Ubuntu/install_kubernetes.sh
@@ -3,3 +3,6 @@
 curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
+
+# dashboard
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
