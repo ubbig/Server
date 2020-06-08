30baac81d912c7569649cfd3339380d99e44c68a
김영준 <yjkim@selab.co.kr>
2020-06-08 12:54:59 +0900

    주석추가

--- Ubuntu/install_ansible.sh
+++ Ubuntu/install_ansible.sh
@@ -4,3 +4,6 @@ sudo apt update
 sudo apt install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
+
+# knownhost추가
+# ~/hosts 파일에 호스트 추가
