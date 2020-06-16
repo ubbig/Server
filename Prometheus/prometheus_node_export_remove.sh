f7855fcf2c544d5a684c4cdfd9c9be3e6d4cf9ba
박태형 <thpark@selab.co.kr>
2020-06-16 17:34:54 +0900

    프로메테우스 삭제 스크립트

---
+++ Prometheus/prometheus_node_export_remove.sh
@@ -0,0 +1,2 @@
+sudo rm -rf ./node_exporter
+sudo rm -rf /etc/systemd/system/node_exporter
---
+++ Prometheus/prometheus_remove.sh
@@ -0,0 +1,7 @@
+sudo rm -rf ./prometheus
+sudo rm -rf /etc/prometheus
+sudo rm -rf /var/lib/prometheus
+sudo rm -rf /etc/systemd/system/prometheus.service
+sudo rm -rf /usr/local/bin/prometheus
+sudo rm -rf /usr/local/bin/prometool
+
