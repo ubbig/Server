ed4213dbf816eec01936d2dc31ca7799849b6e4e
김영준 <yjkim@selab.co.kr>
2020-07-07 14:21:20 +0900

    centos 6.4 docker 1.7설치 스크립트 추가

--- Centos/6.4/install_docker.sh
+++ Centos/6.4/install_docker.sh
@@ -5,3 +5,12 @@ yum clean all
 wget https://get.docker.com/rpm/1.7.1/centos-6/RPMS/x86_64/docker-engine-1.7.1-1.el6.x86_64.rpm
 yum install -y docker-engine-1.7.1-1.el6.x86_64.rpm
 service docker restart
+
+
+systemctl start docker
+systemctl enable docker
+
+groupadd docker
+# 지자기 실행 계정
+usermod -aG docker mod_oper
+newgrp docker
