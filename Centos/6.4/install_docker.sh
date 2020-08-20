73dc5e37c833faece61166d2086a5c890f5986d0
김영준 <yjkim@selab.co.kr>
2020-08-20 18:36:15 +0900

    centos 6.10에서 발생하는 에러 반영함

--- Centos/6.4/install_docker.sh
+++ Centos/6.4/install_docker.sh
@@ -6,11 +6,11 @@ wget https://get.docker.com/rpm/1.7.1/centos-6/RPMS/x86_64/docker-engine-1.7.1-1
 yum install -y docker-engine-1.7.1-1.el6.x86_64.rpm
 service docker restart
 
+chkconfig --add docker
+#systemctl start docker
+#systemctl enable docker
 
-systemctl start docker
-systemctl enable docker
-
-groupadd docker
+#groupadd docker
 # 지자기 실행 계정
-usermod -aG docker mod_oper
-newgrp docker
+#usermod -aG docker mod_oper
+#newgrp docker
