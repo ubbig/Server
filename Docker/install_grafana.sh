f50f070d769994fa57a03a4c05ab3361330a1660
김영준 <yjkim@selab.co.kr>
2020-10-13 14:27:51 +0900

    grafana 로그 레벨을 debug로 설정하는 예제 추가

--- Docker/install_grafana.sh
+++ Docker/install_grafana.sh
@@ -4,4 +4,11 @@ docker pull grafana/grafana:6.5.0-ubuntu
 
 docker volume create grafana-storage
 
+# 로그 레벨을 debug로 설정
+#docker run -d -p 3000:3000 --name grafana -v grafana-storage:/var/lib/grafana \
+#  -e TZ=Asia/Seoul \
+#  -e "GF_LOG_LEVEL=debug" \
+#  grafana/grafana:6.5.0-ubuntu
+
 docker run -d -p 3000:3000 --name grafana -v grafana-storage:/var/lib/grafana -e TZ=Asia/Seoul grafana/grafana:6.5.0-ubuntu
+
