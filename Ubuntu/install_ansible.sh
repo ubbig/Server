cbd290cd322a2628f0ed1c0739faf00e2e9c7210
김영준 <yjkim@selab.co.kr>
2020-10-15 13:27:04 +0900

    ansible 설치 스크립트 centos/ubuntu모두 가능하도록 수정

--- Ubuntu/install_ansible.sh
+++ Ubuntu/install_ansible.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install -y ansible
+is_ubuntu=false
+
+if [ -f /etc/lsb-release ]; then
+  is_ubuntu=true
+fi
+
+if [ "$is_ubuntu" = true ]; then
+  sudo apt update
+  sudo apt install -y software-properties-common
+  sudo apt-add-repository --yes --update ppa:ansible/ansible
+  sudo apt install -y ansible
+else
+  sudo yum install -y epel-release
+  sudo yum install -y ansible
+fi
 
 # knownhost추가
 # ~/hosts 파일에 호스트 추가
+
+
