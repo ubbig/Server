cacd23d6c835fdb0e837abeaaee5b42f280846db
김영준 <yjkim@selab.co.kr>
2020-11-04 19:26:37 +0900

    폴더 경로 생성 스크립트 추가

--- Docker/install_postgresql.sh
+++ Docker/install_postgresql.sh
@@ -6,6 +6,8 @@ docker pull postgres:13.0
 docker network create --driver=bridge postgres-net
 
 docker run -i --rm postgres cat /usr/share/postgresql/postgresql.conf.sample > my-postgres.conf
+
+sudo mkdir -p /docker_data/postgresql/config
 sudo mv my-postgres.conf /docker_data/postgresql/config
 
 docker run -d --restart unless-stopped --name postgresql -p 5432:5432 \
