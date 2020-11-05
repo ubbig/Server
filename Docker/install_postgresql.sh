2d1548ed2f40ac1913fa8706f1b82c9c875b5bd7
김영준 <yjkim@selab.co.kr>
2020-11-05 16:55:00 +0900

    postgresql 설치 스크립트 업데이트  - 초기 계정 지정  - 네트워크 설정 주석처리

--- Docker/install_postgresql.sh
+++ Docker/install_postgresql.sh
@@ -3,18 +3,19 @@
 # https://hub.docker.com/_/postgres
 docker pull postgres:13.0
 
-docker network create --driver=bridge postgres-net
+#docker network create --driver=bridge postgres-net
 
 docker run -i --rm postgres cat /usr/share/postgresql/postgresql.conf.sample > my-postgres.conf
 
 sudo mkdir -p /docker_data/postgresql/config
 sudo mv my-postgres.conf /docker_data/postgresql/config
 
+# 	--network postgres-net \
 docker run -d --restart unless-stopped --name postgresql -p 5432:5432 \
 	-v /docker_data/postgresql/data:/var/lib/postgresql/data \
 	-v /docker_data/postgresql/config:/etc/postgresql/config \
-	-e POSTGRES_PASSWORD='selab' \
-	--network postgres-net \
+	-e POSTGRES_USER=selabdev \
+	-e POSTGRES_PASSWORD=qhdkscjfwj\!@ \
 	postgres:13.0 -c 'config_file=/etc/postgresql/config/my-postgres.conf'
 
 sudo firewall-cmd --permanent --zone=public --add-port=5432/tcp
