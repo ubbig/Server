ca75116d4486d5135fd5aee4b6e806b1579ff54a
김영준 <yjkim@selab.co.kr>
2020-11-06 16:01:04 +0900

    pgadmin 스크립트 추가

---
+++ Docker/postgresql/install_pgadmin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker volume create pgadmin4
+
+docker run -d --restart unless-stopped --name pgadmin \
+  -p 82:80 \
+  -e 'PGADMIN_DEFAULT_EMAIL=selabdev.selab@selab.co.kr' \
+  -e 'PGADMIN_DEFAULT_PASSWORD=qhdkscjfwj!@' \
+  -v pgadmin4:/var/lib/pgadmin \
+  dpage/pgadmin4:4.27
