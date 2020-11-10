64544d862c61781580bbd9205e6793ff80cfc397
김영준 <yjkim@selab.co.kr>
2020-11-10 17:12:48 +0900

    ELK  - 파일경로 변경  - 버전업데이트

--- Docker/elasticsearch/install_elasticsearch.sh
+++ Docker/elasticsearch/install_elasticsearch.sh
@@ -2,17 +2,17 @@
 
 # 공식 도커 이미지 리스트 : https://www.docker.elastic.co/#
 
-docker pull docker.elastic.co/elasticsearch/elasticsearch:7.7.0
-docker pull docker.elastic.co/kibana/kibana:7.7.0
-docker pull docker.elastic.co/beats/filebeat:7.7.0
-docker pull docker.elastic.co/logstash/logstash:7.7.0
+docker pull docker.elastic.co/elasticsearch/elasticsearch:7.9.2
+docker pull docker.elastic.co/kibana/kibana:7.9.2
+docker pull docker.elastic.co/beats/filebeat:7.9.2
+docker pull docker.elastic.co/logstash/logstash:7.9.2
 
-docker run -d --restart unless-stopped --name elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.7.0
-docker run -d --restart unless-stopped --name kibana --link elasticsearch:elasticsearch -p 5601:5601 docker.elastic.co/kibana/kibana:7.7.0
+docker run -d --restart unless-stopped --name elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.9.2
+docker run -d --restart unless-stopped --name kibana --link elasticsearch:elasticsearch -p 5601:5601 docker.elastic.co/kibana/kibana:7.9.2
 #  -e elasticsearch.username=selabdev \
 #  -e elasticsearch.password=qhdkscjfwj\!@ \
 
-docker run -dit --restart unless-stopped --name logstash -v /docker_data/logstash/pipeline/:/usr/share/logstash/pipeline/ docker.elastic.co/logstash/logstash:7.7.0
+docker run -dit --restart unless-stopped --name logstash -v /docker_data/logstash/pipeline/:/usr/share/logstash/pipeline/ docker.elastic.co/logstash/logstash:7.9.2
 
 #docker run -d --restart unless-stopped --name filebeat-qi4a \
 #  --link elasticsearch:elasticsearch \
@@ -20,14 +20,14 @@ docker run -dit --restart unless-stopped --name logstash -v /docker_data/logstas
 #  --volume="/docker_data/filebeat-qi4a/filebeat.docker.yml:/usr/share/filebeat/filebeat.yml:ro" \
 #  --volume="/var/lib/docker/containers:/var/lib/docker/containers:ro" \
 #  --volume="/var/run/docker.sock:/var/run/docker.sock:ro" \
-#docker.elastic.co/beats/filebeat:7.7.0 -e -strict.perms=false \
+#docker.elastic.co/beats/filebeat:7.9.2 -e -strict.perms=false \
 #setup -E setup.kibana.host=kibana:5601 \
 #-E output.elasticsearch.hosts=elasticsearch:9200
 
 docker run -dit --restart unless-stopped --name filebeat-qi4a \
   --link elasticsearch:elasticsearch --link kibana:kibana --link logstash:logstash \
   --volume="/docker_data/filebeat-qi4a/filebeat.docker.yml:/usr/share/filebeat/filebeat.yml:ro" \
-docker.elastic.co/beats/filebeat:7.7.0 -e -strict.perms=false \
+docker.elastic.co/beats/filebeat:7.9.2 -e -strict.perms=false \
 setup -E setup.kibana.host=kibana:5601 \
 -E output.elasticsearch.hosts=elasticsearch:9200 \
 -E output.logstash.hosts=logstash:5044
