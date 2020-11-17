6a68c89331b9369cac5ad31acce0beb15c8dfa01
김영준 <yjkim@selab.co.kr>
2020-11-17 12:56:19 +0900

    도커 볼륨 생성 스크립트 누락된 것 추가

--- Docker/install_jenkins.sh
+++ Docker/install_jenkins.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
 # https://github.com/jenkinsci/docker/blob/master/README.md 참고
 
 docker pull jenkins/jenkins:lts
 
-docker run -d --restart unless-stopped -v jenkins_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 --name jenkins-master jenkins/jenkins:lts
+docker volume create jenkins_home
+
+docker run -d --restart unless-stopped --name jenkins-master  \
+  -v jenkins_home:/var/jenkins_home \
+  -p 8080:8080 -p 50000:50000 \
+  jenkins/jenkins:lts
 
 sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
 sudo firewall-cmd --permanent --zone=public --add-port=50000/tcp
