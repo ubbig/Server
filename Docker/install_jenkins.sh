9ecf822358878a5b0593f0ca9c357a49d25ba8c1
김영준 <yjkim@selab.co.kr>
2020-11-17 13:53:07 +0900

    slave 작업 중 WIP

--- Docker/install_jenkins.sh
+++ Docker/install_jenkins.sh
@@ -14,3 +14,15 @@ docker run -d --restart unless-stopped --name jenkins-master  \
 sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
 sudo firewall-cmd --permanent --zone=public --add-port=50000/tcp
 sudo firewall-cmd --reload
+
+
+#docker pull jenkins/slave:4.6-1
+
+#docker volume create jenkins-agent-workdir
+#docker run -i --rm --name jenkins-agent \
+#  -e TZ=Asia/Seoul \
+#  --init -v jenkins-agent-workdir:/home/jenkins/agent jenkins/agent:latest-jdk11 java -jar /usr/share/jenkins/agent.jar -workDir /home/jenkins/agent
+
+
+#docker container exec -it jenkins-master ssh-keygen -t rsa
+#docker container exec -it jenkins-master cat /var/jenkins_home/.ssh/id_rsa.pub
