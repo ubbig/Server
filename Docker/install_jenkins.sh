48c67e078c214b40e3703323a0e02908f1bee80d
김영준 <yjkim@selab.co.kr>
2020-11-17 13:52:47 +0900

    타임존 설정 누락된 것 모두 추가

--- Docker/install_jenkins.sh
+++ Docker/install_jenkins.sh
@@ -6,6 +6,7 @@ docker pull jenkins/jenkins:lts
 docker volume create jenkins_home
 
 docker run -d --restart unless-stopped --name jenkins-master  \
+  -e TZ=Asia/Seoul \
   -v jenkins_home:/var/jenkins_home \
   -p 8080:8080 -p 50000:50000 \
   jenkins/jenkins:lts
