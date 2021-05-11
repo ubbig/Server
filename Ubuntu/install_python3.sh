242e48c50d724675cd4a408317ebcca68f988c3d
김영준 <yjkim@selab.co.kr>
2021-05-11 17:59:12 +0900

    아나콘다 설치 추가

--- Ubuntu/install_python3.sh
+++ Ubuntu/install_python3.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 sudo apt install -y python3-pip
+sudo apt install -y python3-distutils
+sudo apt install -y python3-venv
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
+
+source ~/.bashrc
+
+python3 -m venv rwp-env
+source rwp-env/bin/activate
