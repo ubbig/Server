dd1b0846d9d03087a2ec4106dd84efa4566976bb
김영준 <yjkim@selab.co.kr>
2021-06-01 15:02:31 +0900

    톰캣 배포 경로 bind설정 추가

--- Docker/install_tomcat9.sh
+++ Docker/install_tomcat9.sh
@@ -2,9 +2,11 @@
 
 docker pull tomcat:9.0-jdk11-adoptopenjdk-hotspot
 
-docker run -dit --restart unless-stopped  \
+docker run -dit --restart unless-stopped --name tomcat9 \
   -e TZ=Asia/Seoul \
-  --name tomcat9 -p 8080:8080 tomcat:9.0-jdk11-adoptopenjdk-hotspot
+  -p 8080:8080 \
+  -v /docker_data/tomcat9/webapps:/usr/local/tomcat/webapps \
+  tomcat:9.0-jdk11-adoptopenjdk-hotspot
 
-sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
-sudo firewall-cmd --reload
+#sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
+#sudo firewall-cmd --reload
