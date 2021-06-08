68a4148f75112d632aa0268fb81732dd915e256e
박태형 <thpark@selab.co.kr>
2021-06-08 19:55:31 +0900

    ssh가 replication 설정할때 설치와 자동 실행되도록 추가

--- Docker/postgresql-replication-script/replication-master-setting.sh
+++ Docker/postgresql-replication-script/replication-master-setting.sh
@@ -1,5 +1,10 @@
 #bin/bash
 
+apt-get update
+apt-get install -y vim
+apt-get install -y ssh
+echo service ssh start >> /root/.bashrc
+
 su - postgres -c "psql -U postgres -d postgres -p 5452 -c \"create user repl replication password 'selab';\""
 
 su - postgres -c "psql -U postgres -d postgres -p 5452 -c \"SELECT * FROM pg_create_physical_replication_slot('repl_slot_01');\""
