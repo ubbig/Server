942f48d395082ff3cddf0a6d201e4c37ba42f483
김영준 <yjkim@selab.co.kr>
2021-06-09 20:50:03 +0900

    ftp build하지 않는 버전으로 스크립트 추가함

---
+++ Docker/ftp/docker_install_ftp_yjkim.sh
@@ -0,0 +1,13 @@
+# https://hub.docker.com/r/fauria/vsftpd
+
+# TODO yjkim : 기본 로그 파일 로그정보가 별로 안남아서 로그 레벨 조정이 필요
+docker run -d --restart unless-stopped --cpus=1 \
+  --name vsftpd \
+  -p 20:20 -p 21:21 -p 21100-21110:21100-21110 \
+  -e PASV_MIN_PORT=21100 \
+  -e PASV_MAX_PORT=21110 \
+  -e TZ=Asia/Seoul \
+  -e FTP_USER=selabdev -e FTP_PASS='password' \
+  -v /docker_data/vsftp/data:/home/vsftpd \
+  -v /docker_data/vsftp/log:/var/log/vsftpd \
+  fauria/vsftpd:latest
