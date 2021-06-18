4b9214dd82dce62e7254f965cbe71052fe6bf109
김영준 <yjkim@selab.co.kr>
2021-06-18 12:05:43 +0900

    docker-compose설치 추가

--- Centos/docker_install.sh
+++ Centos/docker_install.sh
@@ -9,3 +9,10 @@ sudo systemctl enable docker
 sudo groupadd docker
 sudo usermod -aG docker selabdev
 newgrp docker
+
+
+# docker-compose 최신버전 설치
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
--- Ubuntu/20.04/docker_install_Ubuntu20.04.sh
+++ Ubuntu/20.04/docker_install_Ubuntu20.04.sh
@@ -26,5 +26,6 @@ sudo systemctl restart docker
 # docker-compose 최신버전 설치
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
 
