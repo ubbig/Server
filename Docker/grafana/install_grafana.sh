dca851d5b83428b91981d92c3c316a36c66739fa
김영준 <yjkim@selab.co.kr>
2021-06-25 19:00:37 +0900

    버전 업데이트

--- Docker/grafana/install_grafana.sh
+++ Docker/grafana/install_grafana.sh
@@ -13,5 +13,5 @@ docker volume create grafana-storage
 docker run -d --restart unless-stopped -p 3000:3000 --name grafana \
 -v grafana-storage:/var/lib/grafana \
 -e TZ=Asia/Seoul \
-grafana/grafana:6.6.0-ubuntu
+grafana/grafana:8.0.3-ubuntu
 
