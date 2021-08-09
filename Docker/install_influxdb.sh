44b01f012ddc70f52e68e477e201cfa56df5d1f3
김영준 <yjkim@selab.co.kr>
2021-08-09 16:31:35 +0900

    서비스로 등록

--- Docker/install_influxdb.sh
+++ Docker/install_influxdb.sh
@@ -4,9 +4,21 @@
 docker volume create influxdb
 
 # TODO yjkim : 설정파일 연결 추가할 것
-docker run -p 8086:8086 \
+#docker run -p 8086:8086 \
+#  -e TZ=Asia/Seoul \
+#  -v influxdb:/var/lib/influxdb \
+#  -e DOCKER_INFLUXDB_INIT_USERNAME=selabdev \
+#  -e DOCKER_INFLUXDB_INIT_PASSWORD='${_selabdev_password}' \
+#  -e DOCKER_INFLUXDB_INIT_ORG=selab \
+#  -e DOCKER_INFLUXDB_INIT_BUCKET=test-bucket \
+#  influxdb:2.0.7
+
+
+docker service create --name influxdb \
+  --constraint node.hostname==durian-dev  \
+  -p 8086:8086 \
   -e TZ=Asia/Seoul \
-  -v influxdb:/var/lib/influxdb \
+  --mount 'type=volume,src=influxdb,dst=/var/lib/influxdb' \
   -e DOCKER_INFLUXDB_INIT_USERNAME=selabdev \
   -e DOCKER_INFLUXDB_INIT_PASSWORD='${_selabdev_password}' \
   -e DOCKER_INFLUXDB_INIT_ORG=selab \
