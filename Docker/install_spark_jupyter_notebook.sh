2efa7ee2f263896966a0c0c204d175b54b132d4d
김영준 <yjkim@selab.co.kr>
2021-08-11 19:44:33 +0900

    jupyter notebook 주석 추가  - 로그인시 인증토큰이 필요한데 재시작 후 콘솔 로그를 확인하여 토큰 획득 후 로그인 한다.   - 재시작할 때마다 인증토큰은 변경된다.   - 영구 로그인이 가능한 버전을 추후 작업이 필요함

--- Docker/install_spark_jupyter_notebook.sh
+++ Docker/install_spark_jupyter_notebook.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # https://hub.docker.com/r/jupyter/all-spark-notebook
 
+# docker volume create all-spark-notebook
+
 docker run -d --restart unless-stopped --name jupyter \
 -e TZ=Asia/Seoul \
 -p 50081:8888 \
 -v all-spark-notebook:/home/jovyan/work \
 -v /data/etri_sensor_data:/home/jovyan/work/etri_sensor_data \
 192.168.100.7:5000/jupyter/all-spark-notebook
+
+# docker restart jupyter; docker logs -f jupyter
