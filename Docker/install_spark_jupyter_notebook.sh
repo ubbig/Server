2ad3000529c28ec54f0e2ea3959868d72ee312c1
김영준 <yjkim@selab.co.kr>
2021-08-23 13:30:33 +0900

    데이터파일 설치 경로를 SSD로 이동함

--- Docker/install_spark_jupyter_notebook.sh
+++ Docker/install_spark_jupyter_notebook.sh
@@ -7,7 +7,7 @@ docker run -d --restart unless-stopped --name jupyter \
 -e TZ=Asia/Seoul \
 -p 50081:8888 \
 -v all-spark-notebook:/home/jovyan/work \
--v /data/etri_sensor_data:/home/jovyan/work/etri_sensor_data \
+-v /home/selabdev/etri_sensor_data:/home/jovyan/work/etri_sensor_data:ro \
 192.168.100.7:5000/jupyter/all-spark-notebook
 
 # docker restart jupyter; docker logs -f jupyter
