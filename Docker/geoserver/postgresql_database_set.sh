3d6c5bcf5f5e4f7fd5c2186aca10a456b635fe62
김영록 <yrkim@selab.co.kr>
2021-08-27 11:45:06 +0900

    postgresql_database_set

--- Docker/geoserver/postgresql_database_set.sh
+++ Docker/geoserver/postgresql_database_set.sh
@@ -1,4 +1,7 @@
+#database 생성
 psql -U postgres -h 192.168.100.7 -p 5434 -c "CREATE DATABASE landslidencam ENCODING 'UTF-8' LC_COLLATE 'en_US.UTF-8' LC_CTYPE 'en_US.UTF-8'"
+#database 연결
 psql -U postgres -h 192.168.100.7 -p 5434 -c "\connect landslidencam"
+#database 확장자 추가
 psql -U postgres -h 192.168.100.7 -p 5434 -c "CREATE EXTENSION postgis"
 psql -U postgres -h 192.168.100.7 -p 5434 -c "CREATE EXTENSION hstore"
