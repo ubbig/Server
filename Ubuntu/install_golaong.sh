23ce81f9db2c4b96e704114e5b4d7e489e4fd6a1
김영준 <yjkim@selab.co.kr>
2021-10-06 14:58:07 +0900

    go version 업그레이드

--- Ubuntu/install_golaong.sh
+++ Ubuntu/install_golaong.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# v1.15.3
-wget https://golang.org/dl/go1.15.3.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.15.3.linux-amd64.tar.gz
+# v1.17.1
+wget https://golang.org/dl/go1.17.1.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.17.1.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
