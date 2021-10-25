746bf34a4a695c4751fcc23721e147db394890be
김영준 <yjkim@selab.co.kr>
2021-10-25 12:23:03 +0900

    minio설치 스크립트 추가

---
+++ Docker/minio/install_minio.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo mkdir -p /docker_data/minio/data
+
+docker pull minio/minio:RELEASE.2021-10-08T23-58-24Z
+
+docker volume create minio-volume
+
+docker run -d --restart unless-stopped -p 9000:9000 -p 9001:9001 \
+  -e TZ=Asia/Seoul \
+  -e MINIO_ROOT_USER={ACCOUNT} \
+  -e MINIO_ROOT_PASSWORD={PASSWORD} \
+  -v minio-volume:/data  \
+  minio/minio:RELEASE.2021-10-08T23-58-24Z server /data --console-address ":9001"
