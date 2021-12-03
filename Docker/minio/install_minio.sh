be62595cb7f9e667c0a0c06b173194575857b287
김영준 <yjkim@selab.co.kr>
2021-12-03 10:45:30 +0900

    minio 프로메테우스 인증 비활성화 옵션 설정, 그러나 프로메테우스 연동은 안됨

--- Docker/minio/install_minio.sh
+++ Docker/minio/install_minio.sh
@@ -7,7 +7,9 @@ docker pull minio/minio:RELEASE.2021-10-08T23-58-24Z
 docker volume create minio-volume
 
 docker run -d --restart unless-stopped -p 9000:9000 -p 9001:9001 \
+  --name minio-server
   -e TZ=Asia/Seoul \
+  -e MINIO_PROMETHEUS_AUTH_TYPE=public
   -e MINIO_ROOT_USER={ACCOUNT} \
   -e MINIO_ROOT_PASSWORD={PASSWORD} \
   -v minio-volume:/data  \
