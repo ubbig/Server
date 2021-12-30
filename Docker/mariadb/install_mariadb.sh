ee896ad2d89c3bcf439141ccd5671a82a31e256b
김영준 <yjkim@selab.co.kr>
2021-12-30 12:54:12 +0900

    환경 설정 추가

--- Docker/mariadb/install_mariadb.sh
+++ Docker/mariadb/install_mariadb.sh
@@ -7,7 +7,7 @@ docker run -d --restart unless-stopped \
   --name docker_mariadb \
   -p 3306:3306 \
   -v /docker_data/docker_mariadb:/var/lib/mysql \
-  -v /docker_data/docker_mariadb_conf/my.cnf:/etc/mysql/my.cnf \
+  -v /docker_data/docker_mysql:/var/lib/mysql \
   -e MYSQL_ROOT_PASSWORD=selab mariadb:10.4
 
 sudo firewall-cmd --permanent --zone=public --add-port=3306/tcp
