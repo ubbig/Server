d0f8b3e03daa5950d7c43ae7af541fd3704bdd00
박연구 <ygpark@selab.co.kr>
2022-01-06 12:09:29 +0900

    Kubernetes install에 Helm 설치 추가

--- Kubernetes/01_install_k8s.sh
+++ Kubernetes/01_install_k8s.sh
@@ -76,3 +76,8 @@ sudo apt-get install -y lvm2
 
 # for IPVS (MetalLB)
 sudo apt-get install -y ipvsadm
+
+# Install Helm binary
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
