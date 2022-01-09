bfec1e82614330a4982ca989948b292574256b42
박연구 <ygpark@selab.co.kr>
2022-01-10 00:34:20 +0900

    helm repo 추가

--- Kubernetes/openldap/install_openldap.sh
+++ Kubernetes/openldap/install_openldap.sh
@@ -3,6 +3,8 @@
 
 # https://artifacthub.io/packages/helm/helm-openldap/openldap-stack-ha
 
+helm repo add helm-openldap https://jp-gouin.github.io/helm-openldap/
+
 helm install openldap helm-openldap/openldap-stack-ha --version 2.1.6 \
   --namespace openldap \
   --create-namespace \
