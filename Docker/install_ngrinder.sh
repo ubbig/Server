bc4b6d27a402a26b34beb162ff2c6b8cc4733eeb
정보현 <bhjung@selab.co.kr>
2022-08-19 10:38:36 +0900

    [FEA] nGrinder 버전 업그레이드
    3.5.2 -> 3.5.5-p1
    테스트 수행에 Connection reset on each test run(테스트 수행간에 연결 재설정) 옵션을 사용하기 위해 버전 변경
    고부하 테스트 시 서버측 커넥션 풀이 가득차서 Connection refused 발생하는 부분 우회

--- Docker/install_ngrinder.sh
+++ Docker/install_ngrinder.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # https://hub.docker.com/r/ngrinder/controller/
 
-docker pull ngrinder/controller:3.5.2
-docker pull ngrinder/agent:3.5.2
+docker pull ngrinder/controller:3.5.5-p1
+docker pull ngrinder/agent:3.5.5-p1
 
-docker run -d --restart unless-stopped --name ngrinder-controller  \
-  -e TZ=Asia/Seoul \
-  -v /docker_data/ngrinder-controller:/opt/ngrinder-controller \
-  -p 80:80 -p 16001:16001 -p 12000-12009:12000-12009 \
-  ngrinder/controller:3.5.2
+docker run -d --restart unless-stopped --name ngrinder-controller \
+    -e TZ=Asia/Seoul \
+    -v /docker_data/ngrinder-controller:/opt/ngrinder-controller \
+    -p 80:80 -p 16001:16001 -p 12000-12009:12000-12009 \
+    ngrinder/controller:3.5.5-p1
 
 docker run -d --restart unless-stopped --name ngrinder-agent \
   -e TZ=Asia/Seoul \
   --link ngrinder-controller:controller \
-  ngrinder/agent:3.5.2
+  ngrinder/agent:3.5.5-p1
