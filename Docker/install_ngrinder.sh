5758cd8007bdd907a7fb597ad60c782ea688ed0a
정보현 <bhjung@selab.co.kr>
2022-08-29 17:26:43 +0900

    [FEA] nGrinder 노출포트 추가
    일부 포트가 노출 안될 시 별도 머신에서 controller로 못 붙는 현상 확인
    
    Port information:
      80: Default controller web UI port.
      9010-9019: agents connect to the controller cluster thorugh these ports.
      12000-12029: controllers allocate stress tests through these ports.

--- Docker/install_ngrinder.sh
+++ Docker/install_ngrinder.sh
@@ -7,7 +7,7 @@ docker pull ngrinder/agent:3.5.5-p1
 docker run -d --restart unless-stopped --name ngrinder-controller \
     -e TZ=Asia/Seoul \
     -v /docker_data/ngrinder-controller:/opt/ngrinder-controller \
-    -p 80:80 -p 16001:16001 -p 12000-12029:12000-12029 -p 9010-9019 \
+    -p 80:80 -p 16001:16001 -p 12000-12029:12000-12029 -p 9010-9019:9010-9019 \
     ngrinder/controller:3.5.5-p1
 
 docker run -d --restart unless-stopped --name ngrinder-agent \
